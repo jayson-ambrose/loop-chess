c0cdeb296e35600ccd5f2efd5bec044bf7dc573b
jayson-ambrose <jayson.ambrose.87@gmail.com>
2025-09-14 18:31:53 -0600

    board now orients based on player-to-play

--- src/stores/puzzleStore.ts
+++ src/stores/puzzleStore.ts
@@ -1,14 +1,19 @@
 import { create } from 'zustand'
 
-interface PuzzleState {
-    activePuzzleCode: string,
-    puzzleData: {puzzle: {id: string | null}},
-    setPuzzle: (code: string) => void
+type PuzzleState = {
+    puzzleData: {
+        puzzle: {
+            id?: string,
+            solution?: string[]}},
+    setPuzzle: (puzzle: object) => void
 }
 
 export const usePuzzleStore = create<PuzzleState>()((set) => ({
-    activePuzzleCode: '',
-    puzzleData: {puzzle: {id: null}},
-    setPuzzle: (code: string) => {
-        set(() => ({activePuzzleCode: code}))
+    puzzleData: {
+        puzzle: {id: '', solution: []},
+    },
+    setPuzzle: (puzzle: object) => {
+        try {
+            set(() => ({puzzleData: {puzzle}}))}
+        catch (error) {console.error("Error setting puzzle in store:", error)}            
     } }))
